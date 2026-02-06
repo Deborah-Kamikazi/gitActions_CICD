8aa7bed4184b3b29b5b7e84be924a27bf1d241d3
deborah <deborahkamikazi9@gmail>
2026-02-06 09:26:02 +0200

    project setup

---
+++ src/test/java/tech/kamikazi/ci_cd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kamikazi.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
