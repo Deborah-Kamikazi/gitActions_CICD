01ba535c8ae74bb8979c94336958b61f36ff2d1d
deborah <deborahkamikazi9@gmail>
2026-02-06 11:36:32 +0200

    resolving the error

--- src/main/java/tech/kamikazi/ci_cd/TestingCI.java
+++ src/main/java/tech/kamikazi/ci_cd/TestingCI.java
@@ -2,6 +2,6 @@ package tech.kamikazi.ci_cd;
 
 public class TestingCI {
     public static void main(String[] args) {
-        System.out.println("CI compile test")
+        System.out.println("CI compile test");
     }
 }
